3de51f8eb10c9f0fc9e494656c122120b7559a98
KarmaKoders <karmakoders@gmail.com>
2024-06-22 18:02:49 +0530

    New Updates

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -16,9 +16,9 @@ const LeftBanner = () => {
         <h4 className='text-lg font-normal'>WELCOME TO MY WORLD</h4>
         <h1 className='text-6xl font-bold text-white'>
             Hi, I'm
-            <span className='text-designColor capitalize'>{" "}Priyanshu Singh   </span>
+            <span className='text-designColor capitalize'>{" "}Priyanshu Singh</span>
             </h1>
-            <h2 className='text-4xl font-bold text-white'>a<span>{text}</span>
+            <h2 className='text-4xl font-bold text-white'>a{" "}<span>{text}</span>
             <Cursor 
             cursorBlinking="false"
             cursorStyle="|"
